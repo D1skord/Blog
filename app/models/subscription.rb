7a6ecd29cbef2c3434208925a7625804eda9c6dd
D1skord <relog.x@yandex.ru>
2018-05-28 15:37:51 +0300

    Добавлен процент рассылки

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,4 +4,8 @@ class Subscription < ApplicationRecord
   def self.send_daily
     User.daily.each { |user| EventMailer.digest_daily(user).deliver_now }
   end
+
+  def self.confirmation_percent
+    (User.daily.confirmed.count * 1.0) / User.daily.count * 100
+  end
 end
